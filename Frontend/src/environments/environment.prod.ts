35d860d5871363c7640f21055d0981cb1c2fa5f0
vidvidvid <weetopol@gmail.com>
2022-07-16 10:26:32 +0200

    copy the frontend folder from lesson 15
    https://github.com/Encode-Club-Solidity-Bootcamp-June/15-Integration/tree/live-lesson/Project

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  network: 'ropsten',
+  tokenContractAddress: '0xb3D9B15Bb1dd572eD8827122598588A6377a1027',
+  apiAddress: 'http://127.0.0.1:3000/',
+  production: true
+};
